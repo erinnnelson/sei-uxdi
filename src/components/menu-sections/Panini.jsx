1a19d281dc85da1716a7781c11b9c75086fcee2a
XX <xx@XXs-MBP13.local>
2019-07-29 14:22:46 -0400

    section item panini

---
+++ src/components/menu-sections/Panini.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function Panini() {
+  return (
+    <div className="menu-section-item">
+      <div className="section-title">
+        <h2>Panini - Hot Pressed Sandwiches</h2>
+      </div>
+      <div className="section-image">
+      </div>
+      <div className="section-content">
+        <p>Fresh Mozzarella with Tomato & Basil <span>$6.95</span></p>
+        <p>Mozzarella with Tomato & Basil <span>$6.95</span></p>
+        <p>Greek Panini - feta cheese, black olives & tomato <span>$6.95</span></p>
+        <p>Vegetable Panini - with grilled vegetagbles <span>$6.95</span></p>
+        <p>Tuna Panini - with cheese & tomato <span>$7.95</span></p>
+        <p>All Panini served with salad</p>
+      </div>
+    </div>
+  )
+ }
