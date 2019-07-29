6c318232cd442fc198b78cd0234e424915ddbaff
XX <xx@XXs-MBP13.local>
2019-07-29 14:19:21 -0400

    menu item desserts

---
+++ src/components/menu-sections/Desserts.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Desserts() {
+  return (
+    <div className="menu-section-item">
+      <div className="section-title">
+        <h2>Desserts</h2>
+      </div>
+      <div className="section-image">
+      </div>
+      <div className="section-content">
+        <p>Ices <span>$2.00</span></p>
+        <p>Ice Cream <span>$3.00</span></p>
+        <p>Cheesecake <span>$3.75</span></p>
+        <p>Chocolate Cake <span>$3.75</span></p>
+      </div>
+    </div>
+  )
+ }
