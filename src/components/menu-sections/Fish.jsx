09c5a9188b55261d8927f3b5420b6e5a48d343c5
XX <xx@XXs-MBP13.local>
2019-07-29 14:56:14 -0400

    section item fish

---
+++ src/components/menu-sections/Fish.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function Fish() {
+  return (
+    <div className="menu-section-item">
+      <div className="section-title">
+        <h2>Fish</h2>
+      </div>
+      <div className="section-image">
+      </div>
+      <div className="section-content">
+        <p>Fried Whiting Fish (with salad or fries) <span>$8.00</span></p>
+        <p>Salmon Fillet (with salad and baked potato) <span>$15.50</span></p>
+        <p>Fish & Chips (or salad) <span>$8.00</span></p>
+        <p>Fish Parmigiana <span>$8.00</span></p>
+        <p>Fish Sticks & Sauce <span>$5.00</span></p>
+        <p>Fish Sticks & Spaghetti <span>$9.00</span></p>
+      </div>
+    </div>
+  )
+}
