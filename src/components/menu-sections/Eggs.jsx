5fb82c6347061b0211bcc75f85a5e6d1fdead6bf
XX <xx@XXs-MBP13.local>
2019-07-29 14:42:02 -0400

    section item eggs

---
+++ src/components/menu-sections/Eggs.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function Eggs() {
+  return (
+    <div className="menu-section-item">
+      <div className="section-title">
+        <h2>Eggs</h2>
+      </div>
+      <div className="section-image">
+      </div>
+      <div className="section-content">
+        <p>Shakshuka <span>$7.00</span></p>
+        <p>Western Omelet (onions, mushrooms, peppers & cheese) <span>$7.00</span></p>
+        <p>Egg White Omelet (mushrooms, peppers & cheese) <span>$9.00</span></p>
+        <p>Spanish Omelet (mushrooms, onions, peppers & cheese) <span>$7.00</span></p>
+        <p>*All served with salad or fries</p>
+      </div>
+    </div>
+  )
+}
