8b9553fd63373f5e54636dab7a222d63847599f9
XX <xx@XXs-MBP13.local>
2019-07-29 14:47:41 -0400

    section item housespecialities

---
+++ src/components/menu-sections/HouseSpecialties.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default function HouseSpecialties() {
+  return (
+    <div className="menu-section-item">
+      <div className="section-title">
+        <h2>House Specialties</h2>
+      </div>
+      <div className="section-image">
+      </div>
+      <div className="section-content">
+        <p>Calzone (cheese, spinach, or vegetable) <span>$4.50</span></p>
+        <p>Quesadilla <span>$6.00</span></p>
+        <p>Cheese Pretzel <span>$4.50</span></p>
+        <p>Mozzarella Sticks <span>$6.95</span></p>
+        <p>Borekas (cheese, spinach, or potato) <span>$3.50</span></p>
+        <p>Spinach Roll <span>$4.50</span></p>
+        <p>Hippie Roll <span>$4.50</span></p>
+        <p>Blintzes (cheese, potato, or cherry) <span>$5.00</span></p>
+        <p>Potato Pancakes <span>$5.00</span></p>
+        <p>Garlic Knots Parmigiana <span>$4.50</span></p>
+      </div>
+    </div>
+  )
+}
