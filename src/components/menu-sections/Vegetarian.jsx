39062fe9a97ddcd98d24cd15612ea8bb62dc2788
XX <xx@XXs-MBP13.local>
2019-07-29 14:53:39 -0400

    section item vegetarain:

---
+++ src/components/menu-sections/Vegetarian.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Vegetarian() {
+  return (
+    <div className="menu-section-item">
+      <div className="section-title">
+        <h2>Vegetarian</h2>
+      </div>
+      <div className="section-image">
+      </div>
+      <div className="section-content">
+        <p>Vegetarian Burger (with salad or fries) <span>$7.00 - $8.00</span></p>
+        <p>Cigars (vegetarian, potato) <span>$6.50</span></p>
+      </div>
+    </div>
+  )
+ }
