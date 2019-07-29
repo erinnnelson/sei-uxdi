79764a94e54514fa2730fc6bf4152d0128e62d3d
Dwayne T <dect55@gmail.com>
2019-07-29 19:19:37 -0400

    footer-edit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,15 @@ import React from 'react'
 export default () => {
   return (
     <>
-      {/* <h2> Contact Us</h2>
-      <p style={{ "text-align: left"}}>  Benny's Kosher Pizza
-      181-30 Union Turnpike
-        Fresh Meadows, Ny 11366</p>
+      <h2> Contact Us</h2>
 
-      <p>Copyright 2019 | All Rights Reserved</p> */}
+      <div id='foot'>
+        <p> Benny's Kosher Pizza
+          181-30 Union Turnpike
+          Fresh Meadows, Ny 11366</p>
 
+        <p>Copyright 2019 | All Rights Reserved</p>
+      </div>
     </>
   );
 }
