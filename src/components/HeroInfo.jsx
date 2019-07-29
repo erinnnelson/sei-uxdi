778edc7aab97e96cc01acd49cf796f2e09004880
Erinn Nelson <erinn@Erinns-MacBook-Pro.local>
2019-07-29 15:29:30 -0400

    import logo

--- src/components/HeroInfo.jsx
+++ src/components/HeroInfo.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+// import logo from '../assets/logo.png'
 
 
 export default () => {
   return (
     <>
+      {/* <img id="hero-logo" src={logo} alt="Logo" /> */}
       <img id="hero-logo" src="https://www.brandcrowd.com/gallery/brands/pictures/picture1414512966600.jpg" alt="Logo" />
       <p id="hero-contact-info">181-30 Union Turnpike • Fresh Meadows, NY 11366 • 718-969-1928</p> 
       
