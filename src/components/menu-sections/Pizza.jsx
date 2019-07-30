b8d7f4db15e0809e4bed4eff8617d1a2e3ba149e
XX <xx@XXs-MBP13.local>
2019-07-30 13:15:11 -0400

    fixed pizza section's missing cols

--- src/components/menu-sections/Pizza.jsx
+++ src/components/menu-sections/Pizza.jsx
@@ -10,30 +10,30 @@ export default function Pizza() {
       <div className="section-image">
       </div>
       <div className="section-content">
-        <h2 className="section-content-title"><span></span><span>Slice</span> <span> 18" Large Pie</span></h2>
-        <p><span>Regular</span><span>$2.50</span><span>$15.95</span></p>
-        <p><span>Sicilian</span><span>$2.75</span><span>$22.00</span></p>
-        <p><span>Mushroom</span><span>$3.25</span><span>$24.00</span></p>
-        <p><span>Vegetable</span><span>$3.25</span><span>$24.00</span></p>
-        <p><span>Spinach</span><span>$3.25</span><span>$24.00</span></p>
-        <p><span>Eggplant</span><span>$3.25</span><span>$24.00</span></p>
-        <p><span>Broccoli</span><span>$3.25</span><span>$24.00</span></p>
-        <p><span>White</span><span>$3.25</span><span>$24.00</span></p>
-        <p><span>Sauteed Vegeatbles (Cheeseless)</span> <span>$3.25</span><span>$24.00</span></p>
-        <p><span>Ziti</span><span>$3.25</span><span>$24.00</span></p>
-        <p><span>Grandma</span><span>$3.75</span><span>$22.00</span></p>
-        <p><span>Olive (Black or Green)</span><span>$3.25</span><span>$22.00</span></p>
-        <p><span>Extra Cheese</span><span>$3.25</span><span>$22.00</span></p>
-        <p><span>Falafel</span><span>$3.25</span><span>$22.00</span></p>
-        <p><span>Stuffed</span><span>$3.75</span><span>$28.00</span></p>
-        <p><span>Cheese Crust Pie</span><span>$22.00</span></p>
-        <p><span>Fresh Mozzarella, Basil, Tomato</span><span>$3.50</span><span>$26.00</span></p>
-        <p><span>Bruschetta Pizza</span><span>$3.25</span><span>$24.00</span></p>
-        <p><span>Spicy Fries</span><span>$3.25</span><span>$24.00</span></p>
-        <p><span>French Fries</span><span>$3.25</span><span>$24.00</span></p>
-        <p><span>Extra Topping</span><span>$1.00</span><span>$5.00</span></p>
-        <p><span>Pizza Sticks</span><span>$2.25</span><span></span></p>
-        <p><span>Garlic Knots</span><span>$4/1.25</span><span>6/$1.50</span></p>
+        <h2 className="section-content-title"><span></span><span>Slice</span><span> 18" Large Pie</span><span> 10" Deep Dish</span><span> 16" Deep Dish</span></h2>
+        <p><span>Regular</span><span>$2.50</span><span>$15.95</span><span>$6.00</span><span>$18.00</span></p>
+        <p><span>Sicilian</span><span>$2.75</span><span>$22.00</span><span></span><span></span></p>
+        <p><span>Mushroom</span><span>$3.25</span><span>$24.00</span><span>$7.00</span><span></span></p>
+        <p><span>Vegetable</span><span>$3.25</span><span>$24.00</span><span>$7.00</span><span></span></p>
+        <p><span>Spinach</span><span>$3.25</span><span>$24.00</span><span>$7.00</span><span></span></p>
+        <p><span>Eggplant</span><span>$3.25</span><span>$24.00</span><span>$7.00</span><span></span></p>
+        <p><span>Broccoli</span><span>$3.25</span><span>$24.00</span><span>$7.00</span><span></span></p>
+        <p><span>White</span><span>$3.25</span><span>$24.00</span><span>$7.00</span><span></span></p>
+        <p><span>Sauteed Vegeatbles (Cheeseless)</span> <span>$3.25</span><span>$24.00</span><span>$7.00</span><span></span></p>
+        <p><span>Ziti</span><span>$3.25</span><span>$24.00</span><span>$7.00</span><span></span></p>
+        <p><span>Grandma</span><span>$3.75</span><span>$22.00</span><span></span><span></span></p>
+        <p><span>Olive (Black or Green)</span><span>$3.25</span><span>$22.00</span><span>$7.00</span><span></span></p>
+        <p><span>Extra Cheese</span><span>$3.25</span><span>$22.00</span><span>$7.00</span><span></span></p>
+        <p><span>Falafel</span><span>$3.25</span><span>$22.00</span><span>$7.00</span><span></span></p>
+        <p><span>Stuffed</span><span>$3.75</span><span>$28.00</span><span>$7.00</span><span></span></p>
+        <p><span>Cheese Crust</span><span>$22.00</span><span></span><span></span><span></span></p>
+        <p><span>Fresh Mozzarella, Basil, Tomato</span><span>$3.50</span><span>$26.00</span><span>$8.00</span><span></span></p>
+        <p><span>Bruschetta Pizza</span><span>$3.25</span><span>$24.00</span><span>$7.00</span><span></span></p>
+        <p><span>Spicy Fries</span><span>$3.25</span><span>$24.00</span><span></span><span></span></p>
+        <p><span>French Fries</span><span>$3.25</span><span>$24.00</span><span></span><span></span></p>
+        <p><span>Extra Topping</span><span>$1.00</span><span>$5.00</span><span>$1.00</span><span></span></p>
+        <p><span>Pizza Sticks</span><span>$2.25</span><span></span><span></span><span></span></p>
+        <p><span>Garlic Knots</span><span>$4/1.25</span><span>6/$1.50</span><span></span><span></span></p>
       </div>
     </div>
   )
