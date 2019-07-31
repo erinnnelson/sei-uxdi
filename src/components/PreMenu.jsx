0a3da263afd022602dcf11a088a6a6ba77b2c30e
XX <xx@XXs-MBP13.local>
2019-07-31 07:10:52 -0400

    couldnt figure out how to make pre menu non transparent in the background.so i moved it down to body

--- src/components/PreMenu.jsx
+++ src/components/PreMenu.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default () => {
   return (
-    <div id="about">
+    <div className="pre-menu" id="about">
       <h1>Menu</h1>
       <p>Below you will find our entire menu. For quick surfing click on the links on the top of the page for specific food selections. We aim to please and give you the pizza exactly how you like it. Please let us know if you prefer it well done, light or if there is any kind of topping you'd like to add to your pizza. If we have it in our shop, we'll put it on your pizza. We are also open to suggestions for new variations of pies, please share your ideas with Benny.</p>
     </div>
