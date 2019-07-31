61dcc902c66cbd4123c1f3122c23407f52d33ef3
XX <xx@XXs-MBP13.local>
2019-07-31 15:13:43 -0400

    footer roughly donbe

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,33 @@
-import React from 'react'
+import React from 'react';
+import yelp from '../assets/icon-images/yelp.png';
+import phone from '../assets/icon-images/phone.png';
+import email from '../assets/icon-images/email.png';
 
 
 export default () => {
   return (
     <>
-      <h2> Contact Us</h2>
+      <h2 className="footer-title"> Contact Us</h2>
 
-      <div id='foot'>
-        <p> Benny's Kosher Pizza
-          181-30 Union Turnpike
+      <div className="footer-content" id='foot'>
+        <div className="footer-icon-wrapper">
+          <div className="footer-icon footer-icon-email">
+            <img src={email} alt="email logo here" />
+          </div>
+          <div className="footer-icon footer-icon-phone">
+            <img src={phone} alt="phone icon here" />
+          </div>
+          <div className="footer-icon footer-icon-yelp">
+            <img src={yelp} alt="yelp logo here" />
+          </div>
+        </div>
+        <div className="footer-bottom-wrapper">
+        <p className="footer-address"> Benny's Kosher Pizza<br />
+          181-30 Union Turnpike<br />
           Fresh Meadows, Ny 11366</p>
 
-        <p>Copyright 2019 | All Rights Reserved</p>
+          <p className="footer-copyright">Copyright 2019 | All Rights Reserved</p>
+          </div>
       </div>
     </>
   );
