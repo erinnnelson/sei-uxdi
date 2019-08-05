56f130ed2109ae0bfeef6794d16c709971712d03
Erinn Nelson <erinn@Erinns-MBP.fios-router.home>
2019-08-04 20:00:20 -0400

    tablet formatting

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -4,15 +4,16 @@ import React from 'react'
 export default () => {
   return (
     <div id="about">
-      <h1>About Us</h1>
-      <p>Your one stop, neighborhood pizzeria in Fresh Meadows.
-        We offer a full menu of Italian ,Israeli and Mediterranean specialties, as well as tempting dairy ddelights.
-        Our dishes are always made fresh to order, and we are happy to provide for our customers special dietary needs,
-        whether its for personal meal or a full catered menu.  We are under the supervision of Vaad Harabonim of Queens and use
-        Cholov Yisroel and Kemach Yoshon.  We hope to see you at Benny's Kosher Pizza soon! Or, give us a call for free delivery.
-        *Benny's Kosher Pizza on Union Turnpike in Fresh Meadows is not affliated with any other pizza establishments.
-
-      </p>
+      <hr />
+      <h1>ABOUT US</h1>
+      <p>Your one stop, neighborhood pizzeria in Fresh Meadows.</p>
+      <p>We offer a full menu of Italian, Israeli and Mediterranean specialties, as well as tempting dairy delights.</p>
+      <p>Our dishes are always made fresh to order, and we are happy to provide for our customers special dietary needs,
+        whether its for personal meal or a full catered menu.</p>
+      <p>We are under the supervision of Vaad Harabonim of Queens and use
+        Cholov Yisroel and Kemach Yoshon.</p>
+      <p>We hope to see you at Benny's Kosher Pizza soon! Or, give us a call for free delivery.</p>
+      <p>* Benny's Kosher Pizza on Union Turnpike in Fresh Meadows is not affliated with any other pizza establishments.</p>
     </div>
   );
 } 
